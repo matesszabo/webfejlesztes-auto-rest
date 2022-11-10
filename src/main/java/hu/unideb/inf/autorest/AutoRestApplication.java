d2aca7e50c6d0d6302de4d1ea30edd430953c64d
matesszabo <matesszabo@hotmail.com>
2022-11-10 19:24:35 +0100

    modelMapper

--- src/main/java/hu/unideb/inf/autorest/AutoRestApplication.java
+++ src/main/java/hu/unideb/inf/autorest/AutoRestApplication.java
@@ -1,13 +1,19 @@
 package hu.unideb.inf.autorest;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AutoRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRestApplication.class, args);
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/hu/unideb/inf/autorest/Service/impl/AutoServiceImpl.java
+++ src/main/java/hu/unideb/inf/autorest/Service/impl/AutoServiceImpl.java
@@ -1,13 +1,14 @@
 package hu.unideb.inf.autorest.Service.impl;
 
 import hu.unideb.inf.autorest.Dto.AutoDto;
-import hu.unideb.inf.autorest.Dto.TulajdonosDto;
 import hu.unideb.inf.autorest.Entity.AutoEntity;
 import hu.unideb.inf.autorest.Repository.AutoRepository;
 import hu.unideb.inf.autorest.Service.AutoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,15 +17,22 @@ public class AutoServiceImpl implements AutoService {
     @Autowired
     AutoRepository autoRepository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @Override
     public List<AutoDto> findAll() {
-        return null;
+        List<AutoDto> autoDtos = new ArrayList<>();
+        for (AutoEntity autoEntity : autoRepository.findAll()) {
+            autoDtos.add(modelMapper.map(autoEntity, AutoDto.class));
+        }
+        return autoDtos;
     }
 
     @Override
     public AutoDto getById(long id) {
         AutoEntity autoEntity = autoRepository.getReferenceById(id);
-        AutoDto autoDto = new AutoDto();
+        /*AutoDto autoDto = new AutoDto();
         autoDto.setId(autoEntity.getId());
         autoDto.setRendszam(autoEntity.getRendszam());
         autoDto.setMarka(autoEntity.getMarka());
@@ -37,18 +45,23 @@ public class AutoServiceImpl implements AutoService {
                 autoEntity.getTulajdonos().getSzemelyi(),
                 autoEntity.getTulajdonos().getSzuldatum()
 
-        ));
+        ));*/
 
-        return autoDto;
+        AutoDto autoDto1 = modelMapper.map(autoEntity, AutoDto.class);
+
+        return autoDto1;
     }
 
     @Override
     public AutoDto save(AutoDto autoDto) {
-        return null;
+        AutoEntity autoEntity = modelMapper.map(autoDto, AutoEntity.class);
+        autoEntity = autoRepository.save(autoEntity);
+        return modelMapper.map(autoEntity, AutoDto.class);
     }
 
     @Override
     public void delete(long id) {
+        autoRepository.deleteById(id);
 
     }
 }
