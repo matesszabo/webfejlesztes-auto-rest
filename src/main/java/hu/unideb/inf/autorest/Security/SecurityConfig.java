5572b54b80cbdf0b9bedf61dd1b4dd1bc0308e50
matesszabo <matesszabo@hotmail.com>
2022-11-17 18:22:04 +0100

    SecurityConfig

---
+++ src/main/java/hu/unideb/inf/autorest/Security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package hu.unideb.inf.autorest.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+                .antMatchers("/api/**").hasAnyRole("USER", "ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+}
